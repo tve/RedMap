12c349b9c813685c0d115120e9369b58fdba177a
Thorsten von Eicken <tve@rightscale.com>
2018-10-12 18:00:02 -0700

    Clean up merge from upstream

--- worldmap.js
+++ worldmap.js
@@ -61,7 +61,7 @@ module.exports = function(RED) {
                     c.showmenu = node.showmenu;
                     c.panit = node.panit;
                     c.showlayers = node.layers;
-                    node.log("Client connect, sending: " + JSON.stringify({command:c}));
+                    //node.log("Client connect, sending: " + JSON.stringify({command:c}));
                     client.write(JSON.stringify({command:c}));
                 }
             });
